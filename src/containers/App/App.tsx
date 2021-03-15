500ff0aa01289553427ca2342c4cf38a2a4285fb
stban2703 <s.t.ban1401@gmail.com>
2021-03-15 00:54:07 -0500

    Added cat props constant

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { EditOptions } from '../../components/EditOptions/EditOptions';
 
+const catProps = {
+    furType: "noPattern",
+    currentAttribute: "fur"
+}
+
 export const App = () => {
-    
-    const [currentAttribute, setCurrentAttribute] = useState("fur");
+
+    const [currentAttribute, setCurrentAttribute] = useState(catProps.currentAttribute);
 
     return (
         <main>
