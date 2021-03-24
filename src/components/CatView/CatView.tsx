d9f8c33a3842f800c87b004674d031abfab216cc
stban2703 <s.t.ban1401@gmail.com>
2021-03-23 20:01:55 -0500

    Render cat svg through library

--- src/components/CatView/CatView.tsx
+++ src/components/CatView/CatView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CatPropType } from '../../utils/CatPropType';
+import { SvgLoader, SvgProxy } from "react-svgmt";
 import './CatView.css';
 
 interface CatViewProps {
@@ -9,11 +10,13 @@ interface CatViewProps {
 export const CatView: React.FC<CatViewProps> = ({ catProps }) => {
     return (
         <article className="CatView">
+            <SvgLoader path={`${process.env.PUBLIC_URL}/images/render/catsvg.svg`}>
+                
+            </SvgLoader>
             {/*<h4>Atributo: {catProps[0].attribute} = Tipo: {catProps[0].type} y Color: {catProps[0].color}</h4>
             <h4>Atributo: {catProps[1].attribute} = Tipo: {catProps[1].type} y Color: {catProps[1].color}</h4>
             <h4>Atributo: {catProps[2].attribute} = Tipo: {catProps[2].type} y Color: {catProps[2].color}</h4>
     <h4>Atributo: {catProps[3].attribute} = Tipo: {catProps[3].type} y Color: {catProps[3].color}</h4>*/}
-            
         </article>
     )
 }
