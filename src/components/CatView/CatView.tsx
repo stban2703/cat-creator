8bfc7c31768e090e25caba8db5d9b00008e65e95
stban2703 <s.t.ban1401@gmail.com>
2021-04-03 12:45:24 -0500

    Added siamese pattern

--- src/components/CatView/CatView.tsx
+++ src/components/CatView/CatView.tsx
@@ -13,11 +13,13 @@ interface CatViewProps {
 export const CatView: React.FC<CatViewProps> = ({ catProps, currentAttribute }) => {
     const catFurProps = catProps.settings[0];
     const catEyesProps = catProps.settings[1];
+    const shadowSelector = svgSelectors.shadowSelector;
     const furOnlyStrokeSelector = svgSelectors.furOnlyStrokeSelector;
     const furFillAndStrokeSelector = svgSelectors.furFillAndStrokeSelector;
     const eyesFillSelector = svgSelectors.eyesFillSelector;
     const linesPatternSelector = svgSelectors.linesPatternSelector;
     const colorAndWhitePatternSelector = svgSelectors.colorAndWhitePatternSelector;
+    const siamesePatternSelector = svgSelectors.siamesePatternSelector;
     const camera = handleCamera(currentAttribute);
 
     return (
@@ -29,6 +31,10 @@ export const CatView: React.FC<CatViewProps> = ({ catProps, currentAttribute }) 
                 <SvgProxy selector={linesPatternSelector} fill={catFurProps.stroke} display={catFurProps.type === "lines" ? "visible" : "none"} />
                 <SvgProxy selector={"#ear-left-linePattern, #ear-right-linePattern"} stroke={catFurProps.type === "lines" ? "rgba(0, 0, 0, 0.1)" : catFurProps.stroke} />
                 <SvgProxy selector={colorAndWhitePatternSelector} display={catFurProps.type === "colorAndWhite" ? "visible" : "none"} fill={catFurProps.fill==="#ffffff" ? "lightGray" : "#FFFFFF"} />
+                <SvgProxy selector={siamesePatternSelector} fill={"#4B3636"} display={catFurProps.type === "siamese" ? "visible" : "none"} stroke={catFurProps.stroke} />
+                <SvgProxy selector={furOnlyStrokeSelector} fill={catFurProps.type === "siamese" ? "#000000" : catFurProps.stroke} />
+                <SvgProxy selector={"#nose, #ear-left-inside, #ear-right-inside"} fill={catFurProps.type === "siamese" ? "#000000" : "#FFC3F9"}/>
+                <SvgProxy selector={shadowSelector} fill-opacity={catFurProps.type === "siamese" ? "0.25" : "0.1"} />
             </SvgLoader>
         </article>
     )
