5af5861bcaf2f6c520012e219353b0f7f7874ab1
stban2703 <s.t.ban1401@gmail.com>
2021-04-08 14:22:57 -0500

    Added styles to CreatedCats

--- src/components/CatItem/CatItem.tsx
+++ src/components/CatItem/CatItem.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { CatPropsType } from '../../utils/CatPropsType';
+import { CatView } from '../CatView/CatView';
+import "./CatItem.css";
 //import { CatPropsType } from '../../utils/CatPropsType';
 
 interface CatItemProps {
-    id: string,
-    catName: string
+    catProps: CatPropsType;
 }
 
-export const CatItem: React.FC<CatItemProps> = ({ id, catName }) => {
+export const CatItem: React.FC<CatItemProps> = ({ catProps }) => {
     return (
-        <li>
-            <h3>{id}</h3>
-            <p>{catName}</p>
+        <li className="CatItem">
+            <div className="CatItem__preview">
+               
+            </div>
+            <div className="CatItem__info">
+                <h3>{catProps.id}</h3>
+                <p>{catProps.catName}</p>
+            </div>
         </li>
     )
 }
--- src/containers/CreatedCats/CreatedCats.tsx
+++ src/containers/CreatedCats/CreatedCats.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CatItem } from '../../components/CatItem/CatItem';
 import { CatPropsType } from '../../utils/CatPropsType';
+import "./CreatedCats.css";
 
 interface CreatedCatsProps {
     catList: CatPropsType[];
@@ -12,13 +13,13 @@ export const CreatedCats: React.FC<CreatedCatsProps> = ({ catList }) => {
         <article className="CreatedCats" >
             <h2 className="CreatedCats__title">Gatos creados</h2>
             <ul className="CreatedCats__container">
-                {catList.map(({ id, catName }) => {
-                    return <Link to={`/create/${id}`} key={id} >
-                        <CatItem id={id} catName={catName} />
+                {catList.map((elem) => {
+                    return <Link to={`/create/${elem.id}`} key={elem.id} >
+                        <CatItem catProps={elem} />
                     </Link>
                 })}
             </ul>
-            <Link to={`/create/new`}>Crear otro</Link>
+            <Link className="CreatedCats__newBtn" to={`/create/new`}>Crear otro</Link>
         </article>
     )
 }
