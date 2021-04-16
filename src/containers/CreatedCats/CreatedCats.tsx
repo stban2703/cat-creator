6fd4b92e94c0440abcb6a84a1abca7625a27091f
stban2703 <s.t.ban1401@gmail.com>
2021-04-15 22:40:41 -0500

    Changed button text

--- src/containers/CreatedCats/CreatedCats.tsx
+++ src/containers/CreatedCats/CreatedCats.tsx
@@ -14,7 +14,7 @@ export const CreatedCats: React.FC<CreatedCatsProps> = ({ catList, onDeleteCat }
     return (
         <article className="CreatedCats" >
             <h2 className="CreatedCats__title">Gatos creados</h2>
-            <Link className="CreatedCats__newBtn" to={`/create/new`}>Crear otro</Link>
+            <Link className="CreatedCats__newBtn" to={`/create/new`}>Crear gato nuevo</Link>
             <section className="CreatedCats__container">
                 {catList.map((elem) => {
                     const intermediateDeleteCat = () => {
