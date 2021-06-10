484b1a3a700b9961b04307ea2337abf3fc741c15
stban2703 <s.t.ban1401@gmail.com>
2021-06-10 14:46:06 -0500

    Added image to intro

--- src/containers/Intro/Intro.tsx
+++ src/containers/Intro/Intro.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getImageSrcFromUrl } from '../../utils/getImageSrcFromUrl';
 import './Intro.css'
 
 interface IntroProps {
@@ -10,6 +11,7 @@ export const Intro: React.FC<IntroProps> = () => {
     return (
         <article className="Intro">
             <section className="Intro__container">
+                <img className="Intro__backgroundIcon" src={getImageSrcFromUrl("items/catshadowicon")} alt="" />
                 <h1 className="Intro__title" >Cat creator</h1>
                 <section className="Intro__controls">
                     <Link className="Intro__btn" to={`/create/new`}>Crear un gato</Link>
